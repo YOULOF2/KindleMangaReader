67ac8241e9a05d96fa321a1b9fafb50e92d9c8c6
YOULOF2 <youlof123@gmail.com>
2022-11-07 11:06:54 +0200

    Completed version 2

--- src/backend/ConvertToMOBI/__init__.py
+++ src/backend/ConvertToMOBI/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import os
 import shutil
 from uuid import uuid4
+import re
 
 PATH_TO_KINDLEGEN = str(Path(Path(__file__).parent, "kindlegen.exe"))
 
@@ -11,17 +12,19 @@ PATH_TO_KINDLEGEN = str(Path(Path(__file__).parent, "kindlegen.exe"))
 def list_to_mobi(
     input_list: list,
     mobi_name: str,
-):    
+):
+    mobi_name = re.sub(r'[\\/*?:"<>|]', "", mobi_name)
+
     uuid_code = uuid4().hex
     input_dir = str(Path(PATH_TO_TEMP, f"{mobi_name}-{uuid_code}\\"))
     os.makedirs(input_dir)
-    
+
     for index, file in enumerate(input_list):
-        file_name = f"{index} " + file.split('\\')[-1]
-                
+        file_name = f"{index} " + file.split("\\")[-1]
+
         dst_filepath = str(Path(Path(input_dir, file_name)))
         shutil.copy(src=file, dst=dst_filepath)
-    
+
     epub_output_file = str(Path(PATH_TO_TEMP, f"{mobi_name}-as-epub.epub"))
     EPubMaker(
         input_dir=input_dir,
@@ -30,7 +33,7 @@ def list_to_mobi(
         author_name="KindleMangaReader",
         publisher_name="YOULOF2",
     ).make_epub()
-    
+
     command = f'{PATH_TO_KINDLEGEN} "{epub_output_file}" -c0 -o "{mobi_name}.mobi"'
     os.system(command)
     return str(Path(PATH_TO_TEMP, f"{mobi_name}.mobi"))
